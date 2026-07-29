a1858966e443d092394d6bee3d26049f931e9732
Carlos Daniel <cdg7327@gmail.com>
2026-07-28 20:34:45 -0500

    Fixed projects' picture problem V1.0.3

--- src/data/inProgress.js
+++ src/data/inProgress.js
@@ -2,7 +2,7 @@ export const inProgressProjects = [
   {
     id: "lucdex",
     name: "Lucdex",
-    image: "/public/projects/lucdex-project.png",
+    image: "public/projects/lucdex-project.png",
     link: "https://github.com/cdglez04/Lucdex-",
     category: "fullstack",
     stack: ["React", "Vite", "JavaScript", "Python", "Django", "PostgreSQL"],
